4147fc49b3518845114450971b88d82e22864a15
EC2 Default User <ec2-user@ip-172-31-71-26.ec2.internal>
2020-10-13 09:38:59 +0000

    [fix]4-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,6 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
     @book_new = Book.new
   end
 
@@ -43,7 +42,7 @@ class BooksController < ApplicationController
     if @book.update(book_params)
        redirect_to book_path(@book) , notice:"You have updated book successfully."
     else
-      
+
       render 'edit'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:top, :about]
 
   def top
 
@@ -21,6 +22,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user!= current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
